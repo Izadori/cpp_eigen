81d077a7c6e241c91ab39fcea3e7b2088051dbad
izadori <blueocean.okinawa@gmail.com>
2021-11-03 13:05:47 +0900

    [add]gauss-newton

---
+++ gauss_newton/gaussnewton.h
@@ -0,0 +1,97 @@
+//
+// gaussnewton.h
+//
+// Copyright (c) 2021 Izadori
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+
+#ifndef __IZADORI_EIGEN_GAUSSNEWTON_H__
+#define __IZADORI_EIGEN_GAUSSNEWTON_H__
+
+#include <Eigen/Eigen>
+
+//---------------------------------------------
+// 関数fのJacobianを計算する
+//---------------------------------------------
+template <typename Grad>
+Eigen::MatrixXd CalcJacobian(
+    Grad & g, Eigen::VectorXd & a, Eigen::VectorXd & x
+)
+{
+    Eigen::MatrixXd ret;
+
+    ret.resize(x.size(), a.size());
+
+    for (unsigned int i = 0; i < x.size(); i++){
+        auto grad = g(a, x(i));
+        for(unsigned int j = 0; j < a.size(); j++){
+            ret.row(i) = grad;
+        }
+    }
+
+    return ret;
+}
+
+//---------------------------------------------
+// Gauss-Newton法の計算
+//---------------------------------------------
+template <typename Func, typename Grad>
+bool GaussNewton(
+    Func & f, Grad & g, Eigen::VectorXd & a, Eigen::VectorXd & x, Eigen::VectorXd & y,
+    const double eps, const unsigned int max_loop
+)
+{
+    // ループ回数
+    unsigned int loop = 0;
+
+    // 残差二乗和
+    double s_new, s_old = 0;
+
+    while(true){
+        // 関数fのJacobianの計算
+        Eigen::MatrixXd jac = CalcJacobian(g, a, x);
+
+        // 関数fの計算
+        Eigen::VectorXd fx(x.size());
+
+        for(unsigned int i = 0; i < x.size(); i++){
+            fx(i) = f(a, x(i));
+        }
+
+        // 残差ベクトルrの計算
+        Eigen::VectorXd r = fx - y;
+        // 残差二乗和の計算
+        s_new = r.squaredNorm();
+
+        // 収束判定
+        if(std::fabs(s_new - s_old) < std::fabs(eps)){
+            break;
+        }
+
+        s_old = s_new;
+
+        //---------------------------
+        // パラメータaの更新
+        //---------------------------
+
+        // Δaの計算
+        Eigen::MatrixXd h = jac.transpose() * jac;
+        Eigen::VectorXd delta_a = h.ldlt().solve(-jac.transpose() * r);
+
+        // パラメータaの更新
+        a += delta_a;
+
+        // ループ回数のチェック
+        if(loop >= max_loop){
+            break;
+        }
+
+        loop++;
+    }
+
+    return (loop < max_loop);
+}
+
+#endif // __IZADORI_EIGEN_GAUSSNEWTON_H__
